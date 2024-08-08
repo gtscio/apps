a708df430e34cb9e719c078395bed2389f2038d5
Martyn Janes <martyn@obany.com>
2024-08-08 14:40:09 +0200

    Debugging startup

--- apps/workbench-ui/src/routes/+layout.ts
+++ apps/workbench-ui/src/routes/+layout.ts
@@ -1,10 +1,10 @@
 // Copyright 2024 IOTA Stiftung.
 // SPDX-License-Identifier: Apache-2.0.
 import { PUBLIC_WORKBENCH_SERVER_URL } from "$env/static/public";
-// import { init as initApiInformation } from "../stores/apiInformation";
-// import { init as initAuthentication } from "../stores/authentication";
+import { init as initApiInformation } from "../stores/apiInformation";
+import { init as initAuthentication } from "../stores/authentication";
 import { init as initLocales } from "../stores/i18n";
-// import { init as initIdentityProfile } from "../stores/profile";
+import { init as initIdentityProfile } from "../stores/profile";
 
 /**
  * Perform a load.
@@ -14,10 +14,11 @@ import { init as initLocales } from "../stores/i18n";
 export async function load(params: { fetch: typeof window.fetch }): Promise<void> {
 	try {
 		await initLocales(params.fetch);
-		// await initApiInformation(PUBLIC_WORKBENCH_SERVER_URL);
-		// await initAuthentication(PUBLIC_WORKBENCH_SERVER_URL);
-		// await initIdentityProfile(env.PUBLIC_WORKBENCH_SERVER_URL);
+		await initApiInformation(PUBLIC_WORKBENCH_SERVER_URL);
+		await initAuthentication(PUBLIC_WORKBENCH_SERVER_URL);
+		await initIdentityProfile(PUBLIC_WORKBENCH_SERVER_URL);
 	} catch (err) {
+		// eslint-disable-next-line no-console
 		console.error("Error during init load", err);
 	}
 }
