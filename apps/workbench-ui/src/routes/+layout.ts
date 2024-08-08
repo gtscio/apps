31e0c452ddfd5bd5c81103874eda5fe9656b45e3
Martyn Janes <martyn@obany.com>
2024-08-08 14:33:44 +0200

    Debug fetch

--- apps/workbench-ui/src/routes/+layout.ts
+++ apps/workbench-ui/src/routes/+layout.ts
@@ -3,7 +3,7 @@
 import { PUBLIC_WORKBENCH_SERVER_URL } from "$env/static/public";
 // import { init as initApiInformation } from "../stores/apiInformation";
 // import { init as initAuthentication } from "../stores/authentication";
-// import { init as initLocales } from "../stores/i18n";
+import { init as initLocales } from "../stores/i18n";
 // import { init as initIdentityProfile } from "../stores/profile";
 
 /**
@@ -13,7 +13,8 @@ import { PUBLIC_WORKBENCH_SERVER_URL } from "$env/static/public";
  */
 export async function load(params: { fetch: typeof window.fetch }): Promise<void> {
 	console.log("PUBLIC_WORKBENCH_SERVER_URL", PUBLIC_WORKBENCH_SERVER_URL);
-	// await initLocales(params.fetch);
+	console.log("params.fetch", params.fetch);
+	await initLocales(params.fetch);
 	// await initApiInformation(PUBLIC_WORKBENCH_SERVER_URL);
 	// await initAuthentication(PUBLIC_WORKBENCH_SERVER_URL);
 	// await initIdentityProfile(env.PUBLIC_WORKBENCH_SERVER_URL);
