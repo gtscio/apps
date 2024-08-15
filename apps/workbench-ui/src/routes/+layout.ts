a1405327f652d6c14618782ee4e4149a50004796
Martyn Janes <martyn@obany.com>
2024-08-15 10:08:32 +0200

    Use dynamic env vars

--- apps/workbench-ui/src/routes/+layout.ts
+++ apps/workbench-ui/src/routes/+layout.ts
@@ -1,6 +1,6 @@
 // Copyright 2024 IOTA Stiftung.
 // SPDX-License-Identifier: Apache-2.0.
-import { PUBLIC_WORKBENCH_API_URL, PUBLIC_WORKBENCH_DEBUG_LANGUAGES } from "$env/static/public";
+import { env } from "$env/dynamic/public";
 import { Coerce } from "@gtsc/core";
 import { init as initApiInformation } from "../stores/apiInformation";
 import { init as initAuthentication } from "../stores/authentication";
@@ -13,11 +13,11 @@ import { init as initIdentityProfile } from "../stores/profile";
  */
 export async function load(): Promise<void> {
 	try {
-		await initLocales(Coerce.boolean(PUBLIC_WORKBENCH_DEBUG_LANGUAGES) ?? false);
-		await initApiInformation(PUBLIC_WORKBENCH_API_URL);
-		await initAuthentication(PUBLIC_WORKBENCH_API_URL);
-		await initIdentityProfile(PUBLIC_WORKBENCH_API_URL);
-		await initIdentity(PUBLIC_WORKBENCH_API_URL);
+		await initLocales(Coerce.boolean(env.PUBLIC_WORKBENCH_DEBUG_LANGUAGES) ?? false);
+		await initApiInformation(env.PUBLIC_WORKBENCH_API_URL);
+		await initAuthentication(env.PUBLIC_WORKBENCH_API_URL);
+		await initIdentityProfile(env.PUBLIC_WORKBENCH_API_URL);
+		await initIdentity(env.PUBLIC_WORKBENCH_API_URL);
 	} catch (err) {
 		// eslint-disable-next-line no-console
 		console.error("Error during init load", err);
