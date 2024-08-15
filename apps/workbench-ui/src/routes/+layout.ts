2283f59d75b96af4e74366137ca440f321a175e2
Martyn Janes <martyn@obany.com>
2024-08-15 10:40:24 +0200

    Improve error handling

--- apps/workbench-ui/src/routes/+layout.ts
+++ apps/workbench-ui/src/routes/+layout.ts
@@ -1,7 +1,7 @@
 // Copyright 2024 IOTA Stiftung.
 // SPDX-License-Identifier: Apache-2.0.
 import { env } from "$env/dynamic/public";
-import { Coerce, Guards } from "@gtsc/core";
+import { Coerce, ErrorHelper, Guards } from "@gtsc/core";
 import { init as initApiInformation } from "../stores/apiInformation";
 import { init as initAuthentication } from "../stores/authentication";
 import { init as initLocales } from "../stores/i18n";
@@ -13,7 +13,7 @@ import { init as initIdentityProfile } from "../stores/profile";
  */
 export async function load(): Promise<void> {
 	try {
-		Guards.stringValue("Workbench", "PUBLIC_WORKBENCH_API_URL", env.PUBLIC_WORKBENCH_API_URL);
+		Guards.stringValue("init", "PUBLIC_WORKBENCH_API_URL", env.PUBLIC_WORKBENCH_API_URL);
 
 		await initLocales(Coerce.boolean(env.PUBLIC_WORKBENCH_DEBUG_LANGUAGES) ?? false);
 		await initApiInformation(env.PUBLIC_WORKBENCH_API_URL);
@@ -21,7 +21,8 @@ export async function load(): Promise<void> {
 		await initIdentityProfile(env.PUBLIC_WORKBENCH_API_URL);
 		await initIdentity(env.PUBLIC_WORKBENCH_API_URL);
 	} catch (err) {
+		// Nothing else is initialized yet so we need to console log manually
 		// eslint-disable-next-line no-console
-		console.error("Error during init load", err);
+		console.error("Error during initialization", ErrorHelper.formatErrors(err).join("\n"));
 	}
 }
