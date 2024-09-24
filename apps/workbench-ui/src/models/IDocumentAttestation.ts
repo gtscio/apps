ed2bd975b7e7f77abcb59407e470b18a8e633cea
Martyn Janes <martyn@obany.com>
2024-09-24 10:25:32 +0200

    Fix build errors

--- apps/workbench-ui/src/models/IDocumentAttestation.ts
+++ apps/workbench-ui/src/models/IDocumentAttestation.ts
@@ -1,9 +1,11 @@
 // Copyright 2024 IOTA Stiftung.
 // SPDX-License-Identifier: Apache-2.0.
+import type { IJsonLdNodeObject } from "@twin.org/data-json-ld";
+
 /**
  * Model representing data stored in document attestation.
  */
-export interface IDocumentAttestation {
+export interface IDocumentAttestation extends IJsonLdNodeObject {
 	/**
 	 * The id of the item in blob storage.
 	 */
